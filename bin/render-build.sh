d8d8661d4ffbf251182349aa615afcf59390e716
Manh Nguyen <manhxnguyen11@gmail.com>
2025-03-03 21:19:13 -0600

    update script for render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,13 +2,19 @@
 # exit on error
 set -o errexit
 
+# Install Ruby dependencies
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
 
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
 
+# Prepare database (creates if doesn't exist)
+bundle exec rails db:prepare
+
+# Run any pending migrations
 bundle exec rails db:migrate
+
+# Seed the database
+# You might want to make this conditional for production
 bundle exec rails db:seed
